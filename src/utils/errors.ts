8b61561c9c78a7b950311e53e6534556288be08e
Darion Kellar <credkellar@gmail.com>
2026-05-13 16:15:47 -0700

    Update errors.ts

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,11 +1,15 @@
-export class TranslatorError extends Error {
+export class TranslationError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = 'TranslatorError';
+    this.name = 'TranslationError';
+    Object.setPrototypeOf(this, TranslationError.prototype);
   }
 }
 
-export const handleMissingKey = (key: string): string => {
-  console.warn(`[Nad-UI Warning]: Key "${key}" not found.`);
-  return `[MISSING_KEY]: ${key}`;
-};
+export class MissingKeyError extends TranslationError {
+  constructor(key: string, language: string) {
+    super(`Translation key "${key}" is missing for language "${language}".`);
+    this.name = 'MissingKeyError';
+    Object.setPrototypeOf(this, MissingKeyError.prototype);
+  }
+}
